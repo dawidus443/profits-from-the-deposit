97a41a14073d82d235bc61af9a063640fbd20fa2
Dawid Badora <dawidbadora@gmail.com>
2023-01-18 12:16:59 +0100

    Setup

---
+++ src/main/java/task/CC/profitsfromthedeposit/ProfitsFromTheDepositApplication.java
@@ -0,0 +1,13 @@
+package task.CC.profitsfromthedeposit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfitsFromTheDepositApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfitsFromTheDepositApplication.class, args);
+	}
+
+}
---
+++ src/test/java/task/CC/profitsfromthedeposit/ProfitsFromTheDepositApplicationTests.java
@@ -0,0 +1,13 @@
+package task.CC.profitsfromthedeposit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfitsFromTheDepositApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
